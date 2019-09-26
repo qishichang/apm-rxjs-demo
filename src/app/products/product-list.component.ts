f05c808aa8c5da8443d4256e01d81b45cb7f290a
qishichang <qishichang@live.com>
2019-09-27 10:41:41 +1200

    Change to declarative pattern

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
-import { Observable, EMPTY } from 'rxjs';
+import { EMPTY } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { Product } from './product';
@@ -8,24 +8,20 @@ import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
-  products$: Observable<Product[]>;
+  products$ = this.productService.products$.pipe(catchError(err => {
+    this.errorMessage = err;
+    return EMPTY;
+  }));
 
   constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.products$ = this.productService.getProducts()
-      .pipe(catchError(err => {
-        this.errorMessage = err;
-        return EMPTY;
-      }));
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');
