9fbf98e909a71aa9ed378276dd33309c82e9a2d4
qishichang <qishichang@live.com>
2019-10-02 21:32:08 +1300

    Higher-order mapping operators

--- src/app/suppliers/supplier.service.ts
+++ src/app/suppliers/supplier.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { throwError } from 'rxjs';
+import { throwError, of } from 'rxjs';
+import { Supplier } from './supplier';
+import { map, concatMap, tap, mergeMap, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,47 @@ import { throwError } from 'rxjs';
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  suppliersWithMap$ = of(1, 5, 8)
+    .pipe(
+      map(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`)
+    ));
+
+  suppliersWithContactMap$ = of(1, 5, 8)
+    .pipe(
+      tap((id) => console.log('contactMap source observable', id)),
+      concatMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`)
+    ));
+
+  suppliersWithMergeMap$ = of(1, 5, 8)
+    .pipe(
+      tap((id) => console.log('mergeMap source observable', id)),
+      mergeMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`)
+    ));
+
+  suppliersWithSwitchMap$ = of(1, 5, 8)
+    .pipe(
+      tap((id) => console.log('switchMap source observable', id)),
+      switchMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`)
+    ));
+
+  constructor(private http: HttpClient) {
+    // this.suppliersWithMap$
+    //   .subscribe(o => {
+    //     o.subscribe(item => console.log('map result', item));
+    //   });
+
+    this.suppliersWithContactMap$.subscribe(
+      item => console.log('contactMap result', item)
+    );
+
+    this.suppliersWithMergeMap$.subscribe(
+      item => console.log('mergeMap result', item)
+    );
+
+    this.suppliersWithSwitchMap$.subscribe(
+      item => console.log('switchMap result', item)
+    );
+  }
 
   private handleError(err: any) {
     // in a real world app, we may send the server to some remote logging infrastructure
